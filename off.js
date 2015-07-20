edd1216c8cc95a7a86ad09445c21d44a0cf019dd
Arthur Battut <arthur.battut@gmail.com>
2015-07-20 11:04:52 +0200

    update list and binding elements

--- off.js
+++ off.js
@@ -3,19 +3,21 @@
 "use strict";(function(e,t){typeof define=="function"&&define.amd?define([],t):typeof exports=="object"?module.exports=t():e.store=t()})(this,function(){function o(){try{return r in t&&t[r]}catch(e){return!1}}var e={},t=window,n=t.document,r="localStorage",i="script",s;e.disabled=!1,e.version="1.3.17",e.set=function(e,t){},e.get=function(e,t){},e.has=function(t){return e.get(t)!==undefined},e.remove=function(e){},e.clear=function(){},e.transact=function(t,n,r){r==null&&(r=n,n=null),n==null&&(n={});var i=e.get(t,n);r(i),e.set(t,i)},e.getAll=function(){},e.forEach=function(){},e.serialize=function(e){return JSON.stringify(e)},e.deserialize=function(e){if(typeof e!="string")return undefined;try{return JSON.parse(e)}catch(t){return e||undefined}};if(o())s=t[r],e.set=function(t,n){return n===undefined?e.remove(t):(s.setItem(t,e.serialize(n)),n)},e.get=function(t,n){var r=e.deserialize(s.getItem(t));return r===undefined?n:r},e.remove=function(e){s.removeItem(e)},e.clear=function(){s.clear()},e.getAll=function(){var t={};return e.forEach(function(e,n){t[e]=n}),t},e.forEach=function(t){for(var n=0;n<s.length;n++){var r=s.key(n);t(r,e.get(r))}};else if(n.documentElement.addBehavior){var u,a;try{a=new ActiveXObject("htmlfile"),a.open(),a.write("<"+i+">document.w=window</"+i+'><iframe src="/favicon.ico"></iframe>'),a.close(),u=a.w.frames[0].document,s=u.createElement("div")}catch(f){s=n.createElement("div"),u=n.body}var l=function(t){return function(){var n=Array.prototype.slice.call(arguments,0);n.unshift(s),u.appendChild(s),s.addBehavior("#default#userData"),s.load(r);var i=t.apply(e,n);return u.removeChild(s),i}},c=new RegExp("[!\"#$%&'()*+,/\\\\:;<=>?@[\\]^`{|}~]","g"),h=function(e){return e.replace(/^d/,"___$&").replace(c,"___")};e.set=l(function(t,n,i){return n=h(n),i===undefined?e.remove(n):(t.setAttribute(n,e.serialize(i)),t.save(r),i)}),e.get=l(function(t,n,r){n=h(n);var i=e.deserialize(t.getAttribute(n));return i===undefined?r:i}),e.remove=l(function(e,t){t=h(t),e.removeAttribute(t),e.save(r)}),e.clear=l(function(e){var t=e.XMLDocument.documentElement.attributes;e.load(r);while(t.length)e.removeAttribute(t[0].name);e.save(r)}),e.getAll=function(t){var n={};return e.forEach(function(e,t){n[e]=t}),n},e.forEach=l(function(t,n){var r=t.XMLDocument.documentElement.attributes;for(var i=0,s;s=r[i];++i)n(s.name,e.deserialize(t.getAttribute(s.name)))})}try{var p="__storejs__";e.set(p,p),e.get(p)!=p&&(e.disabled=!0),e.remove(p)}catch(f){e.disabled=!0}return e.enabled=!e.disabled,e});
 
 OffTracker = function(options){
-  this.bananas = options.elemsToTrack || [];
+  options = options || {};
+
+  this.bananas = document.querySelectorAll('[data-off]');
+  this.trackName = options.trackName || 'track';
+  this.trackArray = store.get(this.trackName) || [],
   this.init();
+  return this;
 }
 OffTracker.prototype = {
-  bananas: [],
-  trackName: 'track',
-  trackArray: store.get(this.trackName) || [],
   init: function(){
     var _self = this;
     for(var z =0; z < _self.bananas.length; z++){
       var elem = _self.bananas[z],
       recordEvent = function(){
-        var dataClk = elem.dataset,
+        var dataClk = this.dataset,
         eventTracked = {
           created_at: new Date().toString(),
           cat: dataClk.category,
@@ -27,8 +29,8 @@ OffTracker.prototype = {
         store.set(_self.trackName, _self.trackArray);
         return false;
       };
-      elem.addEventListener("click", recordEvent, false);
-      elem.addEventListener("touchend", recordEvent, false);
+      elem.addEventListener("click", recordEvent.bind(elem), false);
+      elem.addEventListener("touchend", recordEvent.bind(elem), false);
     }
   },
   JSON2CSV: function(objArray, label, doubleQuote) {
@@ -83,6 +85,6 @@ OffTracker.prototype = {
   exportData: function(){
     var _self = this;
     var csv = _self.JSON2CSV(_self.trackArray);
-    window.open("data:text/csv;charset=utf-8," + escape(csv))
+    window.open("data:text/csv;charset=utf-8," + escape(csv));
   }
 }
